d8012d20df27719f576b4f4e231af99215f97336
mdwalters <mail@mdwalters.ml>
2023-07-01 16:48:16 -0400

    feat: check currently uses checkmark emoji from blobfox server because standard emoji no work

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ const meower = new Bot();
 const revolt = new Client();
 const mongodb = new MongoClient(process.env.MONGODB_URL);
 const db = mongodb.db("RevowerJS");
-const channel = new Channel(new ChannelCollection(revolt), "01H3XKRYDB3RV5P0EM633C0QXY");
 
 revolt.on("ready", async () => {
     console.info(`Logged in on Revolt as ${revolt.user.username}`);
@@ -20,7 +19,10 @@ meower.onLogin(() => {
 });
 
 meower.onPost(async (u, p, o) => {
-    if (o != null) return;
+    const gc = await db.collection("bridges").findOne({ meower_gc: o });
+    let channel = new Channel(new ChannelCollection(revolt), gc.revolt_channel);
+
+    if (!gc) return;
 
     await channel.sendMessage(`${u}: ${p}`);
 });
@@ -28,11 +30,14 @@ meower.onPost(async (u, p, o) => {
 
 revolt.on("messageCreate", async (message) => {
     const user = await db.collection("users").findOne({ revolt_id: message.authorId });
+    const channel = await db.collection("bridges").findOne({ revolt_channel: message.channelId });
 
     if (message.username == revolt.user.username) return;
+    if (message.content.startsWith("!!")) return;
     if (!user) message.reply("You don't have your account linked!");
 
-    meower.post(`${user.meower_username}: ${message.content}`);
+    await message.react("01GKG7NFRVYKXMN3APJHPM2EW4");
+    meower.post(`${user.meower_username}: ${message.content}`, (channel.meower_gc == "home" ? null : channel.meower_gc));
 });
 
 revolt.loginBot(process.env.REVOLT_TOKEN);

2fc4fd665c088122f9f166bd8859dcc63c4769d0
mdwalters <mail@mdwalters.ml>
2023-07-02 01:42:20 -0400

    feat: "replying"

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import Bot from "meowerbot";
-import { Client, Channel, ChannelCollection } from "revolt.js";
+import { Client, Channel, ChannelCollection, Message, MessageCollection } from "revolt.js";
 import { MongoClient } from "mongodb";
 import * as dotenv from "dotenv";
 
@@ -20,7 +20,7 @@ meower.onLogin(() => {
 
 meower.onPost(async (u, p, o) => {
     const gc = await db.collection("bridges").findOne({ meower_gc: (o == null ? "home" : o) });
-    let channel = new Channel(new ChannelCollection(revolt), gc.revolt_channel);
+    const channel = new Channel(new ChannelCollection(revolt), gc.revolt_channel);
 
     if (!gc) return;
 
@@ -31,6 +31,7 @@ meower.onPost(async (u, p, o) => {
 revolt.on("messageCreate", async (message) => {
     const user = await db.collection("users").findOne({ revolt_id: message.authorId });
     const channel = await db.collection("bridges").findOne({ revolt_channel: message.channelId });
+    // const replies = [""];
 
     if (message.username == revolt.user.username) return;
     if (message.content.startsWith("!!")) {
@@ -41,6 +42,20 @@ revolt.on("messageCreate", async (message) => {
         await message.react("01GKGATQ93ZR2K2901HVV444YC");
         message.reply("You don't have your account linked!");
         return;
+    }
+    if (message.replyIds) {
+        message.reply("Replying to posts don't work yet, however, your post was sent anyways :trol:");
+        /*
+        replies.pop();
+        for (let i in message.replyIds) {
+            const reply_message = new Message(new MessageCollection(revolt), message.replyIds[i]);
+            const reply_user = await db.collection("users").findOne({ revolt_id: reply_message.authorId });
+
+            replies.push(reply_user.meower_username);
+        }
+
+        replies.push(" ");
+        */
     }
 
     await message.react("01GKG7NFRVYKXMN3APJHPM2EW4");

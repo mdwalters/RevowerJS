e879c19703475daf2565e8001833370e1440865c
mdwalters <mdwalters.pm@proton.me>
2023-09-14 21:36:54 -0400

    chore: the denoification, part 4

--- README.md
+++ README.md
@@ -1,12 +1,8 @@
 # RevowerJS
 Port of **[@showierdata9978](https://github.com/showierdata9978)**'s Revower to JavaScript
 ## Running
-> **Note**
-This repo uses pnpm, using RevowerJS with npm, or another package manager may work, but pnpm is reccommended
-
 > **Warning**
 This repo uses Deno, running RevowerJS with NodeJS might not work due to syntax differences.
 ```sh
-pnpm install
-pnpm start
+deno task run
 ```
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-import Bot from "npm:meowerbot";
-import { Client, Channel, ChannelCollection } from "npm:revolt.js"; // We need Message and MessageCollection for replies, but that's commented out for now
-import { MongoClient } from "npm:mongodb";
-import * as dotenv from "npm:dotenv";
+import Bot from "npm:meowerbot@^2023.5.22";
+import {
+    Channel,
+    ChannelCollection,
+    Client,
+} from "npm:revolt.js@^7.0.0-beta.9"; // We need Message and MessageCollection for replies, but that's commented out for now
+import { MongoClient } from "npm:mongodb@^6.1.0";
+import * as dotenv from "npm:dotenv@^16.3.1";
 
 dotenv.config();
 
@@ -19,30 +23,41 @@ meower.onLogin(() => {
 });
 
 meower.onPost(async (u, p, o) => {
-    const gc = await db.collection("bridges").find({ meower_gc: (o == null ? "home" : o) }).toArray();
+    const gc = await db.collection("bridges").find({
+        meower_gc: (o == null ? "home" : o),
+    }).toArray();
     if (!gc) return;
 
     for (let i in gc) {
-        const channel = new Channel(new ChannelCollection(revolt), gc[i].revolt_channel);
+        const channel = new Channel(
+            new ChannelCollection(revolt),
+            gc[i].revolt_channel,
+        );
 
         await channel.sendMessage({
             "content": p,
             "masquerade": {
                 "name": u,
-                "avatar": "https://assets.meower.org/PFP/err.png"
-            }
+                "avatar": "https://assets.meower.org/PFP/err.png",
+            },
         });
     }
 });
 
-
 revolt.on("messageCreate", async (message) => {
-    const user = await db.collection("users").findOne({ revolt_id: message.authorId });
-    const channel = await db.collection("bridges").findOne({ revolt_channel: message.channelId });
+    const user = await db.collection("users").findOne({
+        revolt_id: message.authorId,
+    });
+    const channel = await db.collection("bridges").findOne({
+        revolt_channel: message.channelId,
+    });
     const attachments = [""];
     // const replies = [""];
 
-    if (message.username == revolt.user.username || message.authorId == revolt.user.id) return;
+    if (
+        message.username == revolt.user.username ||
+        message.authorId == revolt.user.id
+    ) return;
     if (!channel) return;
     if (message.content.startsWith("!!")) return;
     if (!user) {
@@ -50,36 +65,36 @@ revolt.on("messageCreate", async (message) => {
         message.reply("You don't have your account linked!");
         return;
     }
-    /* if (message.replyIds) {
-        replies.pop();
-        for (let i in message.replyIds) {
-            const reply_message = new Message(new MessageCollection(revolt), message.replyIds[i]);
-            const reply_user = await db.collection("users").findOne({ revolt_id: reply_message.authorId });
-
-            replies.push(reply_user.meower_username);
-        }
-
-        replies.push(" ");
-    } */
     if (message.attachments) {
         attachments.pop();
         for (let i in message.attachments) {
             const response = await fetch("https://go.meower.org/submit", {
                 method: "post",
                 body: JSON.stringify({ "link": message.attachments[i].url }),
-                headers: { "Authorization": Deno.env.get("MEOWER_URL_SHORTENER_KEY"), "Content-Type": "application/json" }
-            }).then(res => res.json());
-            attachments.push(`[${message.attachments[i].url.split("/")[5]}: ${response.full_url}]`);
+                headers: {
+                    "Authorization": Deno.env.get("MEOWER_URL_SHORTENER_KEY"),
+                    "Content-Type": "application/json",
+                }
+            }).then((res) => res.json());
+            attachments.push(
+                `[${message.attachments[i].url.split("/")[5]}: ${response.full_url}]`,
+            );
         }
         attachments.push("");
     }
 
     await message.react("01GKG7NFRVYKXMN3APJHPM2EW4");
-    meower.post(`${user.meower_username}: ${attachments.join(" ")}${message.content}`, (channel.meower_gc == "home" ? null : channel.meower_gc));
+    meower.post(
+        `${user.meower_username}: ${attachments.join(" ")}${message.content}`,
+        channel.meower_gc == "home" ? null : channel.meower_gc,
+    );
 });
 
 meower.onClose(() => {
-    meower.login(Deno.env.get("MEOWER_USERNAME"), Deno.env.get("MEOWER_PASSWORD"));
+    meower.login(
+        Deno.env.get("MEOWER_USERNAME"),
+        Deno.env.get("MEOWER_PASSWORD"),
+    );
 });
 
 revolt.loginBot(Deno.env.get("REVOLT_TOKEN"));
